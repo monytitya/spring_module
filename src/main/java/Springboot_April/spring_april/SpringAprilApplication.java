cabb10d46b6c35132364e848a94ed8c941f67f6f
monytitya <maotityamony23@mekong.edu.kh>
2026-04-19 23:39:38 +0700

    first to push

---
+++ src/main/java/Springboot_April/spring_april/SpringAprilApplication.java
@@ -0,0 +1,13 @@
+package Springboot_April.spring_april;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAprilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Springboot_April/spring_april/SpringAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package Springboot_April.spring_april;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAprilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
